6a54ba3e90a48b8b1aedcbb92631d661c5d06b4d
Sasha Bayan <SashaBayan@gmail.com>
2015-04-23 17:19:52 -0700

    implements basic binary search tree

---
+++ CH4-Trees-and-Graphs/4.01.js
@@ -0,0 +1,33 @@
+//The code below implements a binary search tree. 
+
+var Tree = function(value){
+
+  this.value = value;
+  this.left = null;
+  this.right = null;
+
+  this.addChild = function(val){
+    var child = new Tree(val);
+    if(child.value < this.value){
+      if(!this.left){
+        this.left = child;
+      } else {
+        this.left.addChild(child.value);
+      }
+    } else if(child.value > this.value) {
+      if(!this.right){
+        this.right = child;
+      } else {
+        this.right.addChild(child.value);
+      }
+    }
+  }
+}
+
+// var a = new Tree(5);
+// a.addChild(2)
+// a.addChild(1)
+// a.addChild(3)
+// a.addChild(7)
+// a.addChild(6)
+// a.addChild(9)
